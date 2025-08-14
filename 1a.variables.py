9214722e2cf1a9cfefe841f8bf254f85b5905d21
EricWanjohiMuturi <emuturi124@gmail.com>
2025-08-15 00:20:20 +0300

    learn variables

---
+++ 1a.variables.py
@@ -0,0 +1,20 @@
+#reasigning variables
+
+name = "Python"
+name = "FastAPI"
+
+print(name)
+
+#Note: do not mix and match quotation marks. (""),('')
+
+#operations with strings
+name2 = "Django"
+print(name * 2) #since name is not an integer, it will output a longer string
+
+#Changing Variable Values
+a = 25
+b = a
+
+print(b) #Therefore the value in a is equal to b
+
+
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # python-for-everyone
 Sharpen your python skills with this repo. 
+
+##1. Variables
+
+A variable is a name for a value. It is used to store a value before it is executed in a function.
+Letters or words are used to define variables. 
+Functions are things that perform actions or calculate outputs(or both).
